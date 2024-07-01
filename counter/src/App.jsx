db44b611ee4988b2b67761e57fe1ca6b8b13d84c
Manish Kumar <narutopandit220@gmail.com>
2024-07-01 21:57:36 +0530

    file with recoil

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,48 +1,67 @@
-import { createContext, useContext, useState } from 'react'
+import React from 'react';
+import { createContext, useContext, useState } from 'react';
 import Card from '@mui/material/Card';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+//first thing
 import {
   RecoilRoot,
   atom,
   selector,
   useRecoilState,
   useRecoilValue,
+  useSetRecoilState
 } from 'recoil';
+ 
+//second thing
+const countState = atom({
+  key: 'countState',
+  default: 0
+});
 
-import './App.css'
-const SetCount = createContext();
 function App() {
-  const [count, setCount] = useState(0);
-  return <SetCount.Provider value={{
-    count:count,
-    setCount:setCount
-  }}>
-    <Box variant='outlined' style={{display:'flex',justifyContent:'center',alignItems:'center',height:'100vh'}}>
-    <Card style={{display:'flex',justifyContent:'space-evenly',border:'2px solid grey',width:'300px',height:'300px',alignItems:"center"}}>
-      <Increase ></Increase>
-      <Count ></Count>
-      <Decrease ></Decrease>
-    </Card>
-  </Box>
-  </SetCount.Provider>
-  
+  return (
+    ///third thing
+    <RecoilRoot>
+      <Box variant='outlined' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <Card style={{ display: 'flex', justifyContent: 'space-evenly', border: '2px solid grey', width: '300px', height: '300px', alignItems: "center" }}>
+          <Increase />
+          <Count />
+          <Decrease />
+        </Card>
+      </Box>
+    </RecoilRoot>
+  );
 }
 
-function Increase(){
-  const {count,setCount} = useContext(SetCount);
- return <Button variant='contained'style={{height:'40px'}} onClick={()=>{
-    setCount(count+1)
-  }}>Increase</Button>
+function Increase() {
+  //fourth thing
+  const setCount = useSetRecoilState(countState);
+  return (
+    <Button variant='contained' style={{ height: '40px' }} onClick={() => {
+      setCount((e) => e + 1);
+    }}>
+      Increase
+    </Button>
+  );
 }
-function Decrease(){
-  const {count,setCount} = useContext(SetCount);
-  return <Button variant='contained'style={{height:'40px'}} onClick={()=>{
-    setCount(count-1)
-  }}>Decrease</Button>
+
+function Decrease() {
+  //4th
+  const setCount = useSetRecoilState(countState);
+  return (
+    <Button variant='contained' style={{ height: '40px' }} onClick={() => {
+      setCount((e) => e - 1);
+    }}>
+      Decrease
+    </Button>
+  );
 }
-function Count(){
-  const {count} = useContext(SetCount);
-  return <h1>{count}</h1>
+
+function Count() {
+  //5th thing
+  const count = useRecoilValue(countState);
+  return <h1>{count}</h1>;
 }
-export default App
+
+export default App;
