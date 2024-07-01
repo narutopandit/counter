15d205c401c6aaa3b07c01ebf68993343c2bc962
Manish Kumar <narutopandit220@gmail.com>
2024-07-01 21:29:04 +0530

    file with context

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { createContext, useContext, useState } from 'react'
 import Card from '@mui/material/Card';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
@@ -11,29 +11,38 @@ import {
 } from 'recoil';
 
 import './App.css'
-
+const SetCount = createContext();
 function App() {
   const [count, setCount] = useState(0);
-  return <Box variant='outlined' style={{display:'flex',justifyContent:'center',alignItems:'center',height:'100vh'}}>
+  return <SetCount.Provider value={{
+    count:count,
+    setCount:setCount
+  }}>
+    <Box variant='outlined' style={{display:'flex',justifyContent:'center',alignItems:'center',height:'100vh'}}>
     <Card style={{display:'flex',justifyContent:'space-evenly',border:'2px solid grey',width:'300px',height:'300px',alignItems:"center"}}>
-      <Increase count={count} setCount={setCount}></Increase>
-      <Count count={count} setCount={setCount}></Count>
-      <Decrease count={count} setCount={setCount}></Decrease>
+      <Increase ></Increase>
+      <Count ></Count>
+      <Decrease ></Decrease>
     </Card>
   </Box>
+  </SetCount.Provider>
+  
 }
 
-function Increase({count,setCount}){
+function Increase(){
+  const {count,setCount} = useContext(SetCount);
  return <Button variant='contained'style={{height:'40px'}} onClick={()=>{
     setCount(count+1)
   }}>Increase</Button>
 }
-function Decrease({count,setCount}){
+function Decrease(){
+  const {count,setCount} = useContext(SetCount);
   return <Button variant='contained'style={{height:'40px'}} onClick={()=>{
     setCount(count-1)
   }}>Decrease</Button>
 }
-function Count({count,setCount}){
+function Count(){
+  const {count} = useContext(SetCount);
   return <h1>{count}</h1>
 }
 export default App
